f3e0ddd138e63dcb526541978f6a438b2ed95d52
CarlesRojas <carlesrojas@outlook.com>
2022-04-30 05:16:59 +0200

    Card Style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,170 @@
 import React from "react";
+import SVG from "react-inlinesvg";
 
-export default function Card() {
-    return <div className="Card"></div>;
+import apriliaBrand from "../resources/icons/brands/aprilia.png";
+import benelliBrand from "../resources/icons/brands/benelli.png";
+import bmwBrand from "../resources/icons/brands/bmw.png";
+import brixonBrand from "../resources/icons/brands/brixon.png";
+import cakeBrand from "../resources/icons/brands/cake.png";
+import cfmotoBrand from "../resources/icons/brands/cfmoto.png";
+import daelimBrand from "../resources/icons/brands/daelim.png";
+import derbiBrand from "../resources/icons/brands/derbi.png";
+import ducatiBrand from "../resources/icons/brands/ducati.png";
+import fanticBrand from "../resources/icons/brands/fantic.png";
+import fbMondialBrand from "../resources/icons/brands/fbMondial.png";
+import fkMotorsBrand from "../resources/icons/brands/fkMotors.png";
+import gasGasBrand from "../resources/icons/brands/gasGas.png";
+import gileraBrand from "../resources/icons/brands/gilera.png";
+import goesBrand from "../resources/icons/brands/goes.png";
+import hanwayBrand from "../resources/icons/brands/hanway.png";
+import harleydavidsonBrand from "../resources/icons/brands/harleyDavidson.png";
+import hondaBrand from "../resources/icons/brands/honda.png";
+import husabergBrand from "../resources/icons/brands/husaberg.png";
+import husqvarnaBrand from "../resources/icons/brands/husqvarna.png";
+import indianBrand from "../resources/icons/brands/indian.png";
+import kawasakiBrand from "../resources/icons/brands/kawasaki.png";
+import keewayBrand from "../resources/icons/brands/keeway.png";
+import ktmBrand from "../resources/icons/brands/ktm.png";
+import kymcoBrand from "../resources/icons/brands/kymco.png";
+import lmlBrand from "../resources/icons/brands/lml.png";
+import macborBrand from "../resources/icons/brands/macbor.png";
+import mashBrand from "../resources/icons/brands/mash.png";
+import mittBrand from "../resources/icons/brands/mitt.png";
+import motoGuzziBrand from "../resources/icons/brands/motoGuzzi.png";
+import motorHispaniaBrand from "../resources/icons/brands/motorHispania.png";
+import mvAgustaBrand from "../resources/icons/brands/mvAgusta.png";
+import orcalBrand from "../resources/icons/brands/orcal.png";
+import peugeotBrand from "../resources/icons/brands/peugeot.png";
+import piaggioBrand from "../resources/icons/brands/piaggio.png";
+import quadroBrand from "../resources/icons/brands/quadro.png";
+import riejuBrand from "../resources/icons/brands/rieju.png";
+import royalEnfieldBrand from "../resources/icons/brands/royalEnfield.png";
+import suzukiBrand from "../resources/icons/brands/suzuki.png";
+import swmBrand from "../resources/icons/brands/swm.png";
+import symBrand from "../resources/icons/brands/sym.png";
+import triumphBrand from "../resources/icons/brands/triumph.png";
+import umBrand from "../resources/icons/brands/um.png";
+import vespaBrand from "../resources/icons/brands/vespa.png";
+import vogeBrand from "../resources/icons/brands/voge.png";
+import yamahaBrand from "../resources/icons/brands/yamaha.png";
+import zontesBrand from "../resources/icons/brands/zontes.png";
+
+import aLicense from "../resources/icons/licenses/a.svg";
+import a1bLicense from "../resources/icons/licenses/a1-b.svg";
+import a2License from "../resources/icons/licenses/a2.svg";
+import amLicense from "../resources/icons/licenses/am.svg";
+
+import classicType from "../resources/icons/type/classic.svg";
+import customType from "../resources/icons/type/custom.svg";
+import maxiScooterType from "../resources/icons/type/maxi-scooter.svg";
+import nakedType from "../resources/icons/type/naked.svg";
+import offRoadType from "../resources/icons/type/off-road.svg";
+import scooterType from "../resources/icons/type/scooter.svg";
+import sportType from "../resources/icons/type/sport.svg";
+import touringType from "../resources/icons/type/touring.svg";
+import trailType from "../resources/icons/type/trail.svg";
+import tresRuedasType from "../resources/icons/type/tres-ruedas.svg";
+
+const BRAND_LOGOS = {
+    aprilia: apriliaBrand,
+    benelli: benelliBrand,
+    bmw: bmwBrand,
+    brixon: brixonBrand,
+    cake: cakeBrand,
+    cfmoto: cfmotoBrand,
+    daelim: daelimBrand,
+    derbi: derbiBrand,
+    ducati: ducatiBrand,
+    fantic: fanticBrand,
+    fbMondial: fbMondialBrand,
+    fkMotors: fkMotorsBrand,
+    gasGas: gasGasBrand,
+    gilera: gileraBrand,
+    goes: goesBrand,
+    hanway: hanwayBrand,
+    harleydavidson: harleydavidsonBrand,
+    honda: hondaBrand,
+    husaberg: husabergBrand,
+    husqvarna: husqvarnaBrand,
+    indian: indianBrand,
+    kawasaki: kawasakiBrand,
+    keeway: keewayBrand,
+    ktm: ktmBrand,
+    kymco: kymcoBrand,
+    lml: lmlBrand,
+    macbor: macborBrand,
+    mash: mashBrand,
+    mitt: mittBrand,
+    motoGuzzi: motoGuzziBrand,
+    motorHispania: motorHispaniaBrand,
+    mvAgusta: mvAgustaBrand,
+    orcal: orcalBrand,
+    peugeot: peugeotBrand,
+    piaggio: piaggioBrand,
+    quadro: quadroBrand,
+    rieju: riejuBrand,
+    royalEnfield: royalEnfieldBrand,
+    suzuki: suzukiBrand,
+    swm: swmBrand,
+    sym: symBrand,
+    triumph: triumphBrand,
+    um: umBrand,
+    vespa: vespaBrand,
+    voge: vogeBrand,
+    yamaha: yamahaBrand,
+    zontes: zontesBrand,
+};
+const LICENCES = { a: aLicense, a1b: a1bLicense, a2: a2License, am: amLicense };
+
+const TYPES = {
+    classic: classicType,
+    custom: customType,
+    maxiScooter: maxiScooterType,
+    naked: nakedType,
+    offRoad: offRoadType,
+    scooter: scooterType,
+    sport: sportType,
+    touring: touringType,
+    trail: trailType,
+    tresRuedas: tresRuedasType,
+};
+
+export default function Card({ data, state, onLike, onPass }) {
+    const { /*id,*/ name, oldPrice, price, licence, cc, type, brand, year, km, image /*, url*/ } = data;
+
+    console.log(LICENCES[licence]);
+    console.log(LICENCES);
+    console.log(licence);
+
+    return (
+        <div className="Card">
+            <img src={image} alt="" className="picture" />
+
+            <div className="info">
+                <div className="row1">
+                    <img src={BRAND_LOGOS[brand]} alt="" className="brandLogo" />
+                    <p className="name">{name}</p>
+                </div>
+
+                <div className="row2">
+                    <p className="cc">{`${cc.toLocaleString("es-ES")} c.c.`}</p>
+                    <p className="km">{`${km.toLocaleString("es-ES")} km`}</p>
+                </div>
+
+                <div className="row3">
+                    <p className="oldPrice">{`${oldPrice.toLocaleString("es-ES")} €`}</p>
+                    <p className="price">{`${price.toLocaleString("es-ES")} €`}</p>
+                </div>
+
+                <div className="row4">
+                    <p className="year">{year}</p>
+                    <SVG className="licence" src={LICENCES[licence]} />
+                    <div className="type">
+                        <SVG className="icon" src={TYPES[type]} />
+                        <p className="name">{type}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,10 +12,10 @@ export default function Home() {
         name: "Yamaha RTX Turbo Pro",
         oldPrice: 10000,
         price: 7500,
-        licence: "A",
+        licence: "a",
         cc: 5000,
-        type: "Scooter",
-        brand: "Yamaha",
+        type: "scooter",
+        brand: "yamaha",
         year: 2021,
         km: 16000,
         image: "https://cdn.wallapop.com/images/10420/bq/00/__/c10420p708798496/i2540823870.jpg?pictureSize=W640",
@@ -27,33 +27,38 @@ export default function Home() {
         name: "Yamaha RTX Turbo Pro",
         oldPrice: 10000,
         price: 7500,
-        licence: "A",
+        licence: "a",
         cc: 5000,
-        type: "Scooter",
-        brand: "Yamaha",
+        type: "scooter",
+        brand: "yamaha",
         year: 2021,
         km: 16000,
         image: "https://cdn.wallapop.com/images/10420/bq/00/__/c10420p708798496/i2540823870.jpg?pictureSize=W640",
         url: "https://mundimoto.com/es/motos-segunda-mano-ocasion/naked/yamaha/mt-01-promo-1nbD8Jpw62QTzAbPFLHe",
     });
 
-    const handleLike = () => {};
+    const handleLike = useThrottle((newIndex) => {
+        console.log("LIKE");
+    }, 300);
 
-    const handlePass = () => {};
+    const handlePass = useThrottle((newIndex) => {
+        console.log("PASS");
+    }, 300);
 
     return (
         <div className="Home">
             <div className="cardContainer">
                 <Card data={firstMoto} state={"front"} onLike={handleLike} onPass={handlePass} />
-                <Card data={secondMoto} state={"back"} onLike={handleLike} onPass={handlePass} />
+                {/* <Card data={secondMoto} state={"back"} onLike={handleLike} onPass={handlePass} /> */}
             </div>
 
             <div className="buttonsContainer">
-                <div className="no">
+                <div className="no" onClick={handlePass}>
                     <SVG className="icon" src={NoIcon} />
                     <p className="name">Pass</p>
                 </div>
-                <div className="yes">
+
+                <div className="yes" onClick={handleLike}>
                     <SVG className="icon" src={YesIcon} />
                     <p className="name">Like</p>
                 </div>
